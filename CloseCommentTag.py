2f535731ce8df642e342eefa8521bc9afe38cf96
Daiki Sato <sato.dik+sourcetree@gmail.com>
2014-08-04 20:27:00 +0900

    Initial Commit

---
+++ CloseCommentTag.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import sublime
+import sublime_plugin
+import re
+
+
+regexAll = re.compile('<([a-zA-Z0-9/ _=;\"\.\-]+)>')
+regexTagname = re.compile('(^[a-zA-Z]+)')
+regexNonCloseTag = re.compile(
+    '^(br|wbr|hr|img|col|base|link|meta|input|keygen|area|param|embed|source|track|command)')
+regexIdname = re.compile('id="([\w\-]+)"')
+regexClassname = re.compile('class="([\w\- _]+)')
+
+
+class CloseCommentTagVars():
+    pass
+
+
+class InsertCommentTagCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+
+        # 文書の初めから現在のキャレット位置までのタグを取得
+        regionLeftOfCursor = self.view.substr(sublime.Region(0, self.view.sel()[0].begin()))
+        tags = regexAll.findall(regionLeftOfCursor)
+        openTags = []
+        currentOpenTag = ''
+        closeCommentTagVars = CloseCommentTagVars()
+        closeTag = '<!-- /%s%s -->'
+
+        # タグの一覧から閉じられていないタグの取得
+        for tag in tags:
+            if tag[0] == '/':
+                currentCloseTag = tag[1:]
+                openTagsLast = "".join(regexTagname.findall(openTags[-1]))
+
+                if currentCloseTag == openTagsLast:
+                    openTags.pop()
+            else:
+                openTags.append(tag)
+
+        # 閉じられていないタグ一覧から閉じタグが必要な直近の要素を取得
+        openTags.reverse()
+
+        for openTag in openTags:
+            if not regexNonCloseTag.findall(openTag):
+                currentOpenTag = openTag
+                break
+
+        # id, class名の取得
+        closeCommentTagVars.tagname = "".join(regexTagname.findall(currentOpenTag))
+        closeCommentTagVars.id = "".join(regexIdname.findall(currentOpenTag))
+        print(closeCommentTagVars.id)
+        closeCommentTagVars.classname = "".join(regexClassname.findall(currentOpenTag))
+        closeCommentTagVars.classname = closeCommentTagVars.classname.replace(' ', '.')
+
+        # キャレットの前にid, class名付きのコメントを挿入
+        if closeCommentTagVars.id:
+            closeCommentTagVars.id = '#' + closeCommentTagVars.id
+
+        if closeCommentTagVars.classname:
+            closeCommentTagVars.classname = '.' + closeCommentTagVars.classname
+
+        self.view.insert(
+            edit,
+            self.view.sel()[0].begin(),
+            closeTag % (closeCommentTagVars.id, closeCommentTagVars.classname))
--- README.md
+++ README.md
@@ -1,4 +1,55 @@
 CloseCommentTag
 ===============
 
+_Insert comment tag like a "&lt;!-- /#id.classname --&gt;" before closing tag._  
 HTMLの閉じタグの前にコメントを入れるプラグイン
+
+## Usage
+
+Before. 
+
+```html
+<div id="hoge" class="fuga">
+Hoge
+</div>
+
+<p id="foo" class="bar test">
+Hoge
+</p>
+```
+
+After
+```html
+<div id="hoge" class="fuga">
+Hoge
+<!-- /#hoge.fuga --></div>
+
+<p id="foo" class="bar test">
+Hoge
+<!-- /#foo.bar.test --></p>
+```
+
+1. Move the caret "&lt;/div&gt;" on top of the "&lt;".
+2. Push <kbd>shift</kbd>+<kbd>command</kbd>+<kbd>,</kbd> or Select "Package Control" -> "CloseCommentTag: Insert comment tag."
+3. Before the closing tag put the "&lt;!-- /#hoge.fuga --&gt;"
+
+## Keymap
+
+- Windows: <kbd>shift</kbd>+<kbd>ctrl</kbd>+<kbd>,</kbd>
+- OSX: <kbd>shift</kbd>+<kbd>command</kbd>+<kbd>,</kbd>
+- Linux: <kbd>shift</kbd>+<kbd>command</kbd>+<kbd>,</kbd>
+
+## Install
+
+1. select "Package Control: Add Repository"
+2. input "https://github.com/Satoh-D/CloseCommentTag"
+3. select "Package Control: Install Package"
+4. input "CloseCommentTag"
+
+## Licence
+
+[MIT License](https://github.com/tcnksm/tool/blob/master/LICENCE) 
+
+## Author
+
+[Sato Daiki(@Satoh_D)](https://twitter.com/Satoh_D)
